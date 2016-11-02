4fd17b67caa837d6097ba62a4b84f7f667836f20
JeppeSigaard <jeppe@smatmonkey.dk>
2016-11-02 18:19:16 +0100

    Koala appends, script.js

--- script/script-min.js
+++ script/script-min.js
@@ -4099,8 +4099,47 @@ $(function(){
 });
 
 
-/* Script compiled using codekit */
-autosize($('textarea'));
+// @koala-append 'assets/jsrender.js';
+// @koala-append 'assets/dropzone.js';
+// @koala-append 'assets/js.cookie.js';
+// @koala-append 'assets/sortable.js';
+// @koala-append 'assets/tinysort.js';
+// @koala-append 'assets/autosize.js';
+
+// @koala-append 'components/json.js';
+// @koala-append 'components/ajax.js';
+// @koala-append 'components/jio-data-img.js';
+// @koala-append 'components/form.js';
+
+
+// @koala-append 'modules/update-pop.js';
+// @koala-append 'modules/autosize.js';
+// @koala-append 'modules/clone-select.js';
+// @koala-append 'modules/sync-scroll.js';
+// @koala-append 'modules/register.js';
+// @koala-append 'modules/front-video.js';
+// @koala-append 'modules/YT_uploadVideo.js';
+// @koala-append 'modules/item-finder.js';
+// @koala-append 'modules/user.js';
+// @koala-append 'modules/range.js';
+// @koala-append 'modules/load-inners.js';
+// @koala-append 'modules/toggle-menu.js';
+// @koala-append 'modules/toggle-aside.js';
+// @koala-append 'modules/toggle-article.js';
+// @koala-append 'modules/article-save-post.js';
+// @koala-append 'modules/js-menu.js';
+// @koala-append 'modules/jsquery.js';
+// @koala-append 'modules/renderModule.js';
+// @koala-append 'modules/flogin.js';
+// @koala-append 'modules/limit.js';
+// @koala-append 'modules/sort-attach.js';
+// @koala-append 'modules/file-upload.js';
+// @koala-append 'modules/article-page.js';
+// @koala-append 'modules/notify.js';
+// @koala-append 'modules/edit.js';
+// @koala-append 'modules/typeselect.js';
+// @koala-append 'modules/article-page-header.js';
+
 
 
 var updatePop = function(id,val){
@@ -4119,6 +4158,9 @@ var updatePop = function(id,val){
     });
 
 }
+
+
+autosize($('textarea'));
 
 
 var cloneSelect = function(){
